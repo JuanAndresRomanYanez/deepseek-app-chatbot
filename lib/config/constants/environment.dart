aca2b22f68551a17f4421f149a3534b6dcc1c4fc
Juan Andrés Román Yáñez <andy_blest@hotmail.com>
2025-04-03 18:46:48 -0400

    backend connections env modified

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -4,9 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Environment {
   static String deepSeekKey = dotenv.env['DEEP_SEEK_KEY'] ?? "No key found";
 
-  // Nueva variable para la URL local
   static String localApiBaseUrl = dotenv.env['LOCAL_API_BASE_URL'] ?? "http://10.0.2.2:8000";
-  // Nota: 10.0.2.2 es para el emulador de Android si el servidor está en tu misma PC.
-  // Si usas un dispositivo físico, pon la IP de tu PC (p. ej. 192.168.0.10). el ipv4 de tu pc
+  // pon la IP de tu PC (p. ej. 192.168.0.10). el ipv4 de tu pc
 
 }
